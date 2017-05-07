78d6e9044fd79b6f24c71def2f3917ca24dc0b5d
root <dmytro@flugel.it>
2017-05-07 16:46:55 +0300

    deleted unneeded files

--- cookbooks/private-registry/recipes/default.rb
+++
@@ -1,5 +0,0 @@
-#
-# Cookbook:: private-registry
-# Recipe:: default
-#
-# Copyright:: 2017, Dmytro Konstantynov, All Rights Reserved.
--- cookbooks/zabbix-agent/recipes/default.rb
+++
@@ -1,5 +0,0 @@
-#
-# Cookbook:: zabbix-agent
-# Recipe:: default
-#
-# Copyright:: 2017, Dmytro Konstantynov, All Rights Reserved.
